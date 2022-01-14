2f8d82457c6dc8b6120d1a5ef84aa01ead2ecee0
emanuel <emanuel.moldovan@gmail.com>
2022-01-14 09:20:50 +0100

    add magit, use use-package, tidy up

--- init.el
+++ init.el
@@ -35,20 +35,20 @@
 (let ((packages
 	'(use-package
 	evil
+	evil-collection
 	projectile
 	helm
 	neotree
 	elixir-mode
 	solarized-theme
 	which-key
+	magit
 	;; Required by evil
 	undo-fu)))
   (mapcar 'straight-use-package packages))
 
 (use-package el-patch :straight t)
 (require 'use-package)
-    
-(evil-mode 1)
 
 ;; Individual package configuration
 
@@ -57,10 +57,19 @@
   :bind ("M-<tab>" . neotree-toggle))
 
 (use-package helm
-  :bind ("M-x" . 'helm-M-x))
+  :bind ("M-x" . 'helm-M-x)
+  :config (helm-mode 1))
+
+(use-package which-key
+  :init (setq which-key-popup-type 'minibuffer)
+  :config (which-key-mode 1))
 
 (use-package evil
-  :init (setq evil-undo-system 'undo-fu))
+  :init (setq evil-want-keybinding nil)
+  :config (evil-mode 1))
+
+(use-package evil-collection
+  :config (evil-collection-init 'magit))
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -69,6 +78,14 @@
 (load-theme 'solarized-light 1)
 
 (set-frame-font (font-spec :family "tamsyn" :size 16))
+
+;; Relative line number
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+
+;; Keybindings TODO
+
+(defvar my-keymap (make-sparse-keymap))
 
 ;; open the config
 (find-file-existing "~/.emacs.d/init.el")
