6baf6c267a3b53e23b1a08f2e9955e248942cd7b
emanuel <emanuel.moldovan@gmail.com>
2022-01-24 15:31:28 +0100

    install evil-collection and company

--- init.el
+++ init.el
@@ -37,12 +37,27 @@
 	evil
 	evil-collection
 	projectile
-	helm
 	neotree
 	elixir-mode
-	solarized-theme
 	which-key
 	magit
+	solarized-theme
+
+	;; Completion at point
+	company
+
+	;; Helm
+	helm
+	helm-company
+
+	general ;; keybinding macros
+
+	;; Language server
+	lsp-mode
+	lsp-ui
+	flycheck
+	dap-mode ;; debugger protocol
+
 	;; Required by evil
 	undo-fu)))
   (mapcar 'straight-use-package packages))
@@ -52,40 +67,53 @@
 
 ;; Individual package configuration
 
-(use-package neotree
-  :init (setq neo-show-hidden-files t)
-  :bind ("M-<tab>" . neotree-toggle))
-
 (use-package helm
-  :bind ("M-x" . 'helm-M-x)
-  :config (helm-mode 1))
+  :bind ("M-x" . 'helm-M-x))
+
+(use-package company
+  :bind ("<tab>" . 'company-indent-or-complete-common)
+  :config (global-company-mode))
+
+(use-package neotree
+  :init (setq
+	 neo-keymap-style 'concise
+	 neo-smart-open 1)
+  :bind ("M-<tab>" . neotree-toggle))
 
 (use-package which-key
   :init (setq which-key-popup-type 'minibuffer)
   :config (which-key-mode 1))
 
 (use-package evil
-  :init (setq evil-want-keybinding nil)
+  :init
+  (setq evil-want-keybinding nil)
   :config (evil-mode 1))
 
 (use-package evil-collection
-  :config (evil-collection-init 'magit))
+  :after evil
+  :init (setq evil-want-integration t
+	      evil-collection-mode-list '(magit neotree dired company))
+  :config (evil-collection-init))
 
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
+(use-package projectile
+  :config (projectile-mode 1)
+  :bind ("C-c p" . projectile-command-map))
 
-(load-theme 'solarized-light 1)
-
-(set-frame-font (font-spec :family "tamsyn" :size 16))
+;; -- UI Resets
 
 ;; Relative line number
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
 
-;; Keybindings TODO
+(load-theme 'solarized-light 1)
 
-(defvar my-keymap (make-sparse-keymap))
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(set-frame-font (font-spec :family "tamsyn" :size 16))
+
+;; Keybindings TODO
 
 ;; open the config
 (find-file-existing "~/.emacs.d/init.el")
